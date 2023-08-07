13d09c1ce3a7fc38185d9fa4d72aff421bb80ab9
miha11-hash <d_mihaela@msn.com>
2023-08-07 14:27:05 +0300

    select option

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const getAllProductsStatic = async (req, res) => {
 //throw new Error("testing new error");
 const search="a";
 //const products = await Product.find({featured: true});
-const products = await Product.find({name: {$regex: search, $options: "i"}});
+const products = await Product.find({name: {$regex: search, $options: "i"}}).select("name price");
 res.status(200).json({msg: "my products", data: products, nbHits: products.length})
 }
 
@@ -13,7 +13,7 @@ res.status(200).json({msg: "my products", data: products, nbHits: products.lengt
 
 const getAllProducts = async (req, res) => {
     //console.log(req.query);
-    const {featured, company, name, sort}=req.query;
+    const {featured, company, name, sort, fields}=req.query;
     const myObj={};
     if(featured) {
         myObj.featured=featured === "true" ? true : false
@@ -28,12 +28,20 @@ const getAllProducts = async (req, res) => {
     }
     
     //const products = await Product.find(req.query);
-    const result = Product.find(myObj);
+    let result = Product.find(myObj);
     if(sort) {
         const sortList = sort.split(",").join(" ");
        result=result.sort(sortList);
        //console.log(sort); (ex: sort name,price - insomnia)
+    } else {
+        result=result.sort("createdAt");
     }
+
+    if(fields) {
+        const fieldsList=fields.split(",").join(" ");
+        result=result.select(fieldsList); //selected fields - company & rating
+    }
+
 const products = await result;
 res.status(200).json({msg: "products route", data: products, nbHits: products.length})
 }
